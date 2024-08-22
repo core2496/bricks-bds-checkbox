e8742cb1ca8e7de74a8e635a09c1bd3e1b82f508
Cesar Huacho <cehuacho24@gmail.com>
2024-08-22 18:28:05 -0500

    Casuísticas del componente

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'bricks-bds-checkbox';
+
+  checkBoxTwo:boolean = true;
+  form!: FormGroup;
+  formValue: any;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      checkBoxThree: [{value:true, disabled:false}],
+    });
+  }
+
+  handleChange($event:CustomEvent):void {
+    alert(`El check ha sido ${$event.detail.checked? 'marcado' : 'desmarcado'}: ${$event.detail.value} - ${$event.detail.checked}`)
+  }
+
+  handleToogleCheckBox():void {
+    this.checkBoxTwo = !this.checkBoxTwo; 
+  }
+
+  handleSubmit():void {
+    this.formValue = this.form.value;
+    console.log(this.formValue)
+  }
+
+ 
+}
